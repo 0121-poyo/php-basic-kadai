01b82022d96db407357338729efe8925db61b4d9
0121-poyo <t71323859@gmail.com>
2025-06-25 23:50:41 +0900

    Add files via upload
    連想配列を作ってキーと値を出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        // 代入
+        $price_card = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+     // 連想配列の値を出力する
+       print_r($price_card);
+        ?>
+    </p>
+</body>
+</html>
